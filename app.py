85c9a29a5a2d44d08bf28bd4c11a5182ce7a5e49
Godai Takashina <azarashi.azarashi.a@gmail.com>
2025-02-20 23:58:09 +0900

    tmp: refactoring (needs fixup)

--- app.py
+++ app.py
@@ -7,11 +7,52 @@ import streamlit as st
 
 
 NUM_SUUPAI = 9
+N_PAIS_TENPAI = 13
 
 
 def pai_list_to_str_1idx(pai_list):
     pai_list_1idx = list(map(lambda x: x + 1, pai_list))
     return "".join(map(str, pai_list_1idx))
+
+
+class PaiList:
+    _pai_list: list[int]
+
+    def __init__(self, pai_list: list[int]):
+        self._pai_list = pai_list
+        self.validate()
+
+    def validate(self) -> None:
+        for x in self._pai_list:
+            if x < 0 or 9 <= x:
+                raise ValueError("牌の数字は1から9の範囲でなければなりません")
+        cnt = [0 for i in range(NUM_SUUPAI)]  # 各牌の出現枚数
+        for x in self._pai_list:
+            cnt[x - 1] += 1
+        for m in cnt:
+            if m > 4:
+                raise ValueError("5枚以上使われている牌があります")
+
+    def __str__(self) -> str:
+        pai_list_1idx = list(map(lambda x: x + 1, self._pai_list))
+        return "".join(map(str, pai_list_1idx))
+
+    def __len__(self) -> int:
+        return len(self._pai_list)
+
+    def to_34_array(self) -> list[int]:
+        """Convert to list of pai ids which is in 0 to 33"""
+        tiles_conv = TilesConverter()
+        hand = tiles_conv.string_to_34_array(sou=str(self))
+        return hand
+
+    @classmethod
+    def from_str(cls, s: str) -> "PaiList":
+        for c in s:
+            if not c.isdigit():
+                raise ValueError("数字以外の入力が含まれています")
+        pai_list = [int(c) - 1 for c in s]  # 1 idx to 0 idx
+        return cls(pai_list)
 
 
 class Hand13:
@@ -33,27 +74,14 @@ class Hand13:
 
 
 class TapaiHand:
-    pai_list: list[int]
+    _pai_list: PaiList
     
-    def __init__(self, pai_list):
-        self.pai_list = pai_list
-        self.validate()
-
-    def validate(self) -> None:
-        for x in self.pai_list:
-            if x < 0 or 9 <= x:
-                raise ValueError("牌の数字は1から9の範囲でなければなりません")
-        # 各牌の出現枚数
-        cnt = [0 for i in range(NUM_SUUPAI)]
-        for x in self.pai_list:
-            cnt[x - 1] += 1
-        for m in cnt:
-            if m > 4:
-                raise ValueError("5枚以上使われている牌があります")
+    def __init__(self, pai_list: PaiList):
+        self._pai_list = pai_list
 
     def calc_best_sutepai(self) -> pd.DataFrame:
         hand_len = len(self.pai_list)
-        num_stepai = hand_len - 13
+        num_stepai = hand_len - N_PAIS_TENPAI
         results = []
         known_sutepai = set()
         for sutepai_idx in itertools.combinations(range(hand_len), num_stepai):
@@ -64,7 +92,7 @@ class TapaiHand:
                     sutepai_list.append(pai)
                 else:
                     hand_13.append(pai)
-            assert len(hand_13) == 13
+            assert len(hand_13) == N_PAIS_TENPAI
             sutepai_tuple = tuple(sutepai_list)
             if sutepai_tuple in known_sutepai:
                 continue
@@ -83,6 +111,11 @@ class TapaiHand:
             ascending=False,
         )
         return df
+
+    @classmethod
+    def from_str(cls, s: str) -> "TapaiHand":
+        pai_list = PaiList.from_str(s)
+        return cls(pai_list)
 
 
 def parse_hand(s: str) -> TapaiHand:
